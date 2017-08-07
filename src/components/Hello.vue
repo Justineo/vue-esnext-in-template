a68744d8dc6fa5a3b0bd7cc8d11b5c16cbe301d8
Justineo <justice360@gmail.com>
2017-08-07 16:13:57 +0800

    first commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,22 @@
+<template>
+<div :class="{ a: true, ...b }">Test</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return { b: true }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.a {
+  color: blue;
+}
+
+.b {
+  font-size: 30px;
+}
+</style>
